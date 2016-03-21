365c2b3609f222b4d590f6e8972d98a1f422f629
aleksiy325 <aleksiy123@gmail.com>
2016-03-20 21:24:19 -0700

    bumped fit score to 90%

--- tester/example_bots/protobot/java/foldbot_java.py
+++
@@ -1,8 +0,0 @@
-import os
-this_dir = os.path.dirname(os.path.realpath( __file__ ))
-
-from proto_bot import ProtoBot
-
-class FoldBotJava(ProtoBot):
-    command = ['java', '-jar', os.path.join(this_dir, 'foldbot/dist/foldbot.jar')]
-    output_stderr = True
--- tester/example_bots/raise_twenty_bot.py
+++
@@ -1,5 +0,0 @@
-from bot import Bot
-
-class RaiseTwentyBot(Bot):
-    def turn(self):
-        return self.action('raise', amount=20)
